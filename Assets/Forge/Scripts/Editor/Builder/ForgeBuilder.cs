ebd01ec918f08c55b8a7ad8deef7504f681910c8
Radu13 <raduazzodjantony@libero.it>
2026-04-12 17:01:12 +0200

    Fix culture-dependent formatting and float precision in sky.asset generation

--- Assets/Forge/Scripts/Editor/Builder/ForgeBuilder.cs
+++ Assets/Forge/Scripts/Editor/Builder/ForgeBuilder.cs
@@ -2,6 +2,7 @@ using GLTFast.Export;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Threading;
@@ -17,6 +18,15 @@ using UnityEngine.UIElements;
 
 public static class ForgeBuilder
 {
+    static ForgeBuilder()
+    {
+        var culture = CultureInfo.InvariantCulture;
+        CultureInfo.DefaultThreadCurrentCulture = culture;
+        CultureInfo.DefaultThreadCurrentUICulture = culture;
+        Thread.CurrentThread.CurrentCulture = culture;
+        Thread.CurrentThread.CurrentUICulture = culture;
+    }
+
     static readonly (int RacVersion, GameRegion Region)[] BUILD_VERSIONS = new (int RacVersion, GameRegion Region)[]
     {
         (RCVER.DL, GameRegion.NTSC),
@@ -638,8 +648,8 @@ public static class ForgeBuilder
 
                     writer.WriteLine($"\t\tSkyShell {i} {{");
                     writer.WriteLine($"\t\t\tbloom: {(layer.Bloom ? "true" : "false")}");
-                    writer.WriteLine($"\t\t\tstarting_rotation: [{euler.y} {euler.x} {euler.z}]");
-                    writer.WriteLine($"\t\t\tangular_velocity: [{angvel.y} {angvel.x} {angvel.z}]");
+                    writer.WriteLine($"\t\t\tstarting_rotation: [{euler.y:G9} {euler.x:G9} {euler.z:G9}]");
+                    writer.WriteLine($"\t\t\tangular_velocity: [{angvel.y:G9} {angvel.x:G9} {angvel.z:G9}]");
                     writer.WriteLine($"");
                     writer.WriteLine("\t\t\tMesh mesh {");
                     writer.WriteLine($"\t\t\t\tname: \"{mesh.name}\"");
