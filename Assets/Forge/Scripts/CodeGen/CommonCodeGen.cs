34ff5004b1287961ae3b5eb676bd3347ad807800
Daniel Gerendasy <danielgerendasy@gmail.com>
2026-05-23 00:20:21 -0700

    feat(codegen): add per custom moby toggle to CommonCodeGen

--- Assets/Forge/Scripts/CodeGen/CommonCodeGen.cs
+++ Assets/Forge/Scripts/CodeGen/CommonCodeGen.cs
@@ -27,6 +27,23 @@ public class CommonCodeGen : MonoBehaviour, ICodeGen, IBuildHook
     public bool IsEnabled => true;
     public int CodeGenOrder => 0;
 
+	[Header("Custom Mobys")]
+	public bool CheckpointEnabled = true;
+	public bool GateEnabled = true;
+	public bool MessagerEnabled = true;
+	public bool MoverEnabled = true;
+	public bool PlatformEnabled = true;
+	public bool ControllerEnabled = true;
+	public bool LaserbeamEnabled = true;
+	public bool LaserEnabled = true;
+	public bool PvarPokeEnabled = true;
+	public bool HackerorbEnabled = true;
+	public bool RadarBlipEnabled = true;
+	public bool HealthProxyEnabled = true;
+	public bool LaunchStreamEnabled = true;
+	public bool HoldableProxyEnabled = true;
+	public bool SoulCollectorEnabled = true;
+
     public void Configure(string buildFolder, CodeGenState state)
     {
         var mapConfig = FindObjectOfType<MapConfig>();
@@ -38,99 +55,153 @@ public class CommonCodeGen : MonoBehaviour, ICodeGen, IBuildHook
         // copy base code
         CodeManager.CopySourceFilesIntoWorkingDirectory(FolderNames.GetCodeGenFolder(RCVER.DL, "common"), buildFolder);
 
-        state.ObjectFiles.Add($"{FolderNames.CodeBuildSrcFolder}/checkpoint.o");
-        state.ObjectFiles.Add($"{FolderNames.CodeBuildSrcFolder}/gate.o");
-        state.ObjectFiles.Add($"{FolderNames.CodeBuildSrcFolder}/messager.o");
-        state.ObjectFiles.Add($"{FolderNames.CodeBuildSrcFolder}/mover.o");
-        state.ObjectFiles.Add($"{FolderNames.CodeBuildSrcFolder}/platform.o");
-        state.ObjectFiles.Add($"{FolderNames.CodeBuildSrcFolder}/controller.o");
-        state.ObjectFiles.Add($"{FolderNames.CodeBuildSrcFolder}/laserbeam.o");
-        state.ObjectFiles.Add($"{FolderNames.CodeBuildSrcFolder}/laser.o");
-        state.ObjectFiles.Add($"{FolderNames.CodeBuildSrcFolder}/pvarpoke.o");
         state.ObjectFiles.Add($"{FolderNames.CodeBuildSrcFolder}/maputils.o");
-        state.ObjectFiles.Add($"{FolderNames.CodeBuildSrcFolder}/hackerorb.o");
-        state.ObjectFiles.Add($"{FolderNames.CodeBuildSrcFolder}/blip.o");
-        state.ObjectFiles.Add($"{FolderNames.CodeBuildSrcFolder}/dummy.o");
-        state.ObjectFiles.Add($"{FolderNames.CodeBuildSrcFolder}/launchstream.o");
-        state.ObjectFiles.Add($"{FolderNames.CodeBuildSrcFolder}/holder.o");
-        state.ObjectFiles.Add($"{FolderNames.CodeBuildSrcFolder}/soulcollector.o");
         state.ObjectFiles.Add($"{FolderNames.CodeBuildSrcFolder}/window.o");
 
         state.LDFlags.Add("-DGATE");
         state.LDFlags.Add("-DSOULCOLLECTOR");
 
-        state.Includes.Add("#include \"checkpoint.h\"");
         state.Includes.Add("#include \"maputils.h\"");
-        state.Includes.Add("#include \"gate.h\"");
-        state.Includes.Add("#include \"messager.h\"");
-        state.Includes.Add("#include \"laser.h\"");
-        state.Includes.Add("#include \"controller.h\"");
-        state.Includes.Add("#include \"pvarpoke.h\"");
-        state.Includes.Add("#include \"mover.h\"");
-        state.Includes.Add("#include \"platform.h\"");
-        state.Includes.Add("#include \"hackerorb.h\"");
-        state.Includes.Add("#include \"blip.h\"");
-        state.Includes.Add("#include \"dummy.h\"");
-        state.Includes.Add("#include \"launchstream.h\"");
-        state.Includes.Add("#include \"holder.h\"");
-        state.Includes.Add("#include \"soulcollector.h\"");
 
-        state.Functions.Add($"//--------------------------------------------------------------------------\r\nvoid onBeforeUpdateHeroes(void)\r\n{{\r\n  gateSetCollision(1);\r\n  ((void (*)())0x005ce1d8)();\r\n}}\r\n");
-        state.Functions.Add($"//--------------------------------------------------------------------------\r\nvoid onBeforeUpdateHeroes2(u32 a0)\r\n{{\r\n  gateSetCollision(1);\r\n  ((void (*)(u32))0x0059b320)(a0);\r\n}}\r\n");
+        if (CheckpointEnabled)
+        {
+            state.ObjectFiles.Add($"{FolderNames.CodeBuildSrcFolder}/checkpoint.o");
+            state.Includes.Add("#include \"checkpoint.h\"");
+            state.InitBody.Add($"checkpointInit();");
+            state.GetGuberCase.Add("case CHECKPOINT_MANAGER_OCLASS: return checkpointGetGuber(moby);");
+            state.GetGuberCase.Add("case CHECKPOINT_OCLASS: return checkpointGetGuber(moby);");
+            state.HandleGuberEventCase.Add("case CHECKPOINT_MANAGER_OCLASS: checkpointHandleEvent(moby, event); break;");
+            state.HandleGuberEventCase.Add("case CHECKPOINT_OCLASS: checkpointHandleEvent(moby, event); break;");
+            state.MainBodyReady.Add("checkpointStart();");
+        }
 
-        state.InitBody.Add($"checkpointInit();");
-        state.InitBody.Add($"moverInit();");
-        state.InitBody.Add($"platformInit();");
-        state.InitBody.Add($"controllerInit();");
-        state.InitBody.Add($"gateInit();");
-        state.InitBody.Add($"messagerInit();");
-        state.InitBody.Add($"laserInit();");
-        state.InitBody.Add($"pvarpokeInit();");
-        state.InitBody.Add($"hackerorbInit();");
-        state.InitBody.Add($"blipInit();");
-        state.InitBody.Add($"dummyInit();");
-        state.InitBody.Add($"launchstreamInit();");
-        state.InitBody.Add($"holderInit();");
-        state.InitBody.Add($"soulcollectorInit();");
+        if (MoverEnabled)
+        {
+            state.ObjectFiles.Add($"{FolderNames.CodeBuildSrcFolder}/mover.o");
+            state.Includes.Add("#include \"mover.h\"");
+            state.InitBody.Add($"moverInit();");
+            state.GetGuberCase.Add("case MOVER_OCLASS: return moverGetGuber(moby);");
+            state.HandleGuberEventCase.Add("case MOVER_OCLASS: moverHandleEvent(moby, event); break;");
+            state.MainBodyReady.Add("moverStart();");
+        }
 
-        // get gubers
-        state.GetGuberCase.Add("case CHECKPOINT_MANAGER_OCLASS: return checkpointGetGuber(moby);");
-        state.GetGuberCase.Add("case CHECKPOINT_OCLASS: return checkpointGetGuber(moby);");
-        state.GetGuberCase.Add("case PLATFORM_FLIPPER_MOBY_OCLASS: return platformGetGuber(moby);");
-        state.GetGuberCase.Add("case PLATFORM_PIVOT_MOBY_OCLASS: return platformGetGuber(moby);");
-        state.GetGuberCase.Add("case GATE_OCLASS: return gateGetGuber(moby);");
-        state.GetGuberCase.Add("case MOVER_OCLASS: return moverGetGuber(moby);");
-        state.GetGuberCase.Add("case CONTROLLER_OCLASS: return controllerGetGuber(moby);");
-        state.GetGuberCase.Add("case DUMMY_OCLASS: return dummyGetGuber(moby);");
-        state.GetGuberCase.Add("case MOBY_ID_HACKER_ORB: return hackerorbGetGuber(moby);");
-        state.GetGuberCase.Add("case HOLDER_OCLASS: return holderGetGuber(moby);");
-        state.GetGuberCase.Add("case SOULCOLLECTOR_OCLASS: return soulcollectorGetGuber(moby);");
+        if (PlatformEnabled)
+        {
+            state.ObjectFiles.Add($"{FolderNames.CodeBuildSrcFolder}/platform.o");
+            state.Includes.Add("#include \"platform.h\"");
+            state.InitBody.Add($"platformInit();");
+            state.GetGuberCase.Add("case PLATFORM_FLIPPER_MOBY_OCLASS: return platformGetGuber(moby);");
+            state.GetGuberCase.Add("case PLATFORM_PIVOT_MOBY_OCLASS: return platformGetGuber(moby);");
+            state.HandleGuberEventCase.Add("case PLATFORM_FLIPPER_MOBY_OCLASS: platformHandleEvent(moby, event); break;");
+            state.HandleGuberEventCase.Add("case PLATFORM_PIVOT_MOBY_OCLASS: platformHandleEvent(moby, event); break;");
+        }
 
-        // handle events
-        state.HandleGuberEventCase.Add("case CHECKPOINT_MANAGER_OCLASS: checkpointHandleEvent(moby, event); break;");
-        state.HandleGuberEventCase.Add("case CHECKPOINT_OCLASS: checkpointHandleEvent(moby, event); break;");
-        state.HandleGuberEventCase.Add("case PLATFORM_FLIPPER_MOBY_OCLASS: platformHandleEvent(moby, event); break;");
-        state.HandleGuberEventCase.Add("case PLATFORM_PIVOT_MOBY_OCLASS: platformHandleEvent(moby, event); break;");
-        state.HandleGuberEventCase.Add("case GATE_OCLASS: gateHandleEvent(moby, event); break;");
-        state.HandleGuberEventCase.Add("case MOVER_OCLASS: moverHandleEvent(moby, event); break;");
-        state.HandleGuberEventCase.Add("case CONTROLLER_OCLASS: controllerHandleEvent(moby, event); break;");
-        state.HandleGuberEventCase.Add("case DUMMY_OCLASS: dummyHandleEvent(moby, event); break;");
-        state.HandleGuberEventCase.Add("case MOBY_ID_HACKER_ORB: hackerorbHandleEvent(moby, event); break;");
-        state.HandleGuberEventCase.Add("case HOLDER_OCLASS: holderHandleEvent(moby, event); break;");
-        state.HandleGuberEventCase.Add("case SOULCOLLECTOR_OCLASS: soulcollectorHandleEvent(moby, event); break;");
+        if (ControllerEnabled)
+        {
+            state.ObjectFiles.Add($"{FolderNames.CodeBuildSrcFolder}/controller.o");
+            state.Includes.Add("#include \"controller.h\"");
+            state.InitBody.Add($"controllerInit();");
+            state.GetGuberCase.Add("case CONTROLLER_OCLASS: return controllerGetGuber(moby);");
+            state.HandleGuberEventCase.Add("case CONTROLLER_OCLASS: controllerHandleEvent(moby, event); break;");
+            state.MainBodyReady.Add("controllerStart();");
+        }
 
-        state.InitBody.Add($"HOOK_JAL(0x003bd854, &onBeforeUpdateHeroes);");
-        state.InitBody.Add($"HOOK_JAL(0x0051f648, &onBeforeUpdateHeroes2);");
+        if (GateEnabled)
+        {
+            state.ObjectFiles.Add($"{FolderNames.CodeBuildSrcFolder}/gate.o");
+            state.Includes.Add("#include \"gate.h\"");
+            state.Functions.Add($"//--------------------------------------------------------------------------\r\nvoid onBeforeUpdateHeroes(void)\r\n{{\r\n  gateSetCollision(1);\r\n  ((void (*)())0x005ce1d8)();\r\n}}\r\n");
+            state.Functions.Add($"//--------------------------------------------------------------------------\r\nvoid onBeforeUpdateHeroes2(u32 a0)\r\n{{\r\n  gateSetCollision(1);\r\n  ((void (*)(u32))0x0059b320)(a0);\r\n}}\r\n");
+            state.InitBody.Add($"gateInit();");
+            state.InitBody.Add($"HOOK_JAL(0x003bd854, &onBeforeUpdateHeroes);");
+            state.InitBody.Add($"HOOK_JAL(0x0051f648, &onBeforeUpdateHeroes2);");
+            state.GetGuberCase.Add("case GATE_OCLASS: return gateGetGuber(moby);");
+            state.HandleGuberEventCase.Add("case GATE_OCLASS: gateHandleEvent(moby, event); break;");
+            state.MainBodyReady.Add("gateStart();");
+        }
 
-        state.MainBodyReady.Add("checkpointStart();");
-        state.MainBodyReady.Add("moverStart();");
-        state.MainBodyReady.Add("controllerStart();");
-        state.MainBodyReady.Add("gateStart();");
-        state.MainBodyReady.Add("laserStart();");
-        state.MainBodyReady.Add("pvarpokeStart();");
-        state.MainBodyReady.Add("dummyStart();");
-        state.MainBodyReady.Add("holderStart();");
-        state.MainBodyReady.Add("soulcollectorStart();");
+        if (MessagerEnabled)
+        {
+            state.ObjectFiles.Add($"{FolderNames.CodeBuildSrcFolder}/messager.o");
+            state.Includes.Add("#include \"messager.h\"");
+            state.InitBody.Add($"messagerInit();");
+        }
+
+        if (LaserbeamEnabled || LaserEnabled)
+        {
+            state.ObjectFiles.Add($"{FolderNames.CodeBuildSrcFolder}/laserbeam.o");
+            state.Includes.Add("#include \"laserbeam.h\"");
+        }
+
+        if (LaserEnabled)
+        {
+            state.ObjectFiles.Add($"{FolderNames.CodeBuildSrcFolder}/laser.o");
+            state.Includes.Add("#include \"laser.h\"");
+            state.InitBody.Add($"laserInit();");
+            state.MainBodyReady.Add("laserStart();");
+        }
+
+        if (PvarPokeEnabled)
+        {
+            state.ObjectFiles.Add($"{FolderNames.CodeBuildSrcFolder}/pvarpoke.o");
+            state.Includes.Add("#include \"pvarpoke.h\"");
+            state.InitBody.Add($"pvarpokeInit();");
+            state.MainBodyReady.Add("pvarpokeStart();");
+        }
+
+        if (HackerorbEnabled)
+        {
+            state.ObjectFiles.Add($"{FolderNames.CodeBuildSrcFolder}/hackerorb.o");
+            state.Includes.Add("#include \"hackerorb.h\"");
+            state.InitBody.Add($"hackerorbInit();");
+            state.GetGuberCase.Add("case MOBY_ID_HACKER_ORB: return hackerorbGetGuber(moby);");
+            state.HandleGuberEventCase.Add("case MOBY_ID_HACKER_ORB: hackerorbHandleEvent(moby, event); break;");
+        }
+
+        if (RadarBlipEnabled)
+        {
+            state.ObjectFiles.Add($"{FolderNames.CodeBuildSrcFolder}/blip.o");
+            state.Includes.Add("#include \"blip.h\"");
+            state.InitBody.Add($"blipInit();");
+        }
+
+        if (HealthProxyEnabled)
+        {
+            state.ObjectFiles.Add($"{FolderNames.CodeBuildSrcFolder}/dummy.o");
+            state.Includes.Add("#include \"dummy.h\"");
+            state.InitBody.Add($"dummyInit();");
+            state.GetGuberCase.Add("case DUMMY_OCLASS: return dummyGetGuber(moby);");
+            state.HandleGuberEventCase.Add("case DUMMY_OCLASS: dummyHandleEvent(moby, event); break;");
+            state.MainBodyReady.Add("dummyStart();");
+        }
+
+        if (LaunchStreamEnabled)
+        {
+            state.ObjectFiles.Add($"{FolderNames.CodeBuildSrcFolder}/launchstream.o");
+            state.Includes.Add("#include \"launchstream.h\"");
+            state.InitBody.Add($"launchstreamInit();");
+        }
+
+        if (HoldableProxyEnabled)
+        {
+            state.ObjectFiles.Add($"{FolderNames.CodeBuildSrcFolder}/holder.o");
+            state.Includes.Add("#include \"holder.h\"");
+            state.InitBody.Add($"holderInit();");
+            state.GetGuberCase.Add("case HOLDER_OCLASS: return holderGetGuber(moby);");
+            state.HandleGuberEventCase.Add("case HOLDER_OCLASS: holderHandleEvent(moby, event); break;");
+            state.MainBodyReady.Add("holderStart();");
+        }
+
+        if (SoulCollectorEnabled)
+        {
+            state.ObjectFiles.Add($"{FolderNames.CodeBuildSrcFolder}/soulcollector.o");
+            state.Includes.Add("#include \"soulcollector.h\"");
+            state.InitBody.Add($"soulcollectorInit();");
+            state.GetGuberCase.Add("case SOULCOLLECTOR_OCLASS: return soulcollectorGetGuber(moby);");
+            state.HandleGuberEventCase.Add("case SOULCOLLECTOR_OCLASS: soulcollectorHandleEvent(moby, event); break;");
+            state.MainBodyReady.Add("soulcollectorStart();");
+        }
+
     }
 
     public void Configure(BuildState state, BuildStateStage stage)
