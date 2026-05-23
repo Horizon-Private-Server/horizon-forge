13245c5191e99facb971d682a1e8d9540976c13d
Daniel Gerendasy <Dnawrkshp@users.noreply.github.com>
2026-05-23 09:00:43 -0700

    v2.4.6

--- Assets/Forge/Scripts/Constants.cs
+++ Assets/Forge/Scripts/Constants.cs
@@ -557,7 +557,7 @@ public enum DLMusicTracks
 
 public static class Constants
 {
-    public static readonly string ForgeVersion = "v2.4.5";
+    public static readonly string ForgeVersion = "v2.4.6";
 
     public static readonly string RepoUrl = "https://github.com/Horizon-Private-Server/horizon-forge";
     public static readonly string WikiUrl = $"{RepoUrl}/wiki";
